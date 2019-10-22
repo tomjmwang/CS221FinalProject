25e66b1b34a8c424bfde46472aaceb4982ae18b0
tomjmwang <tomjmwang@yahoo.com>
2019-10-22 16:27:33 -0700

    baseline implementation

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 
 import random
+import collections
 
 
 
@@ -76,7 +77,7 @@ class Game:
         for j,c in enumerate(cards):
             if c[1] == 1:
                 cards[j] = (c[0], 2)
-                print("player", i, "lost card", c[0])
+                #print("player", i, "lost card", c[0])
                 break
         all_cards[i] = tuple(cards)
         return (tuple(all_cards), state[1], state[2], state[3])
@@ -101,10 +102,10 @@ class Game:
         new_state = state
         if name == "tax":
             new_coins[i] += 3
-            print("player", i, "gained 3 coins")
+            #print("player", i, "gained 3 coins")
         elif name == "foreign_aid":
             new_coins[i] += 2
-            print("player", i, "gained 2 coins")
+            #print("player", i, "gained 2 coins")
         elif name == "steal":
             if new_coins[j] >= 2:
                 new_coins[j] -= 2
@@ -112,12 +113,13 @@ class Game:
             else:
                 new_coins[i] += new_coins[j]
                 new_coins[j] = 0
-            print("player", i, "stealed from", j)
+            #print("player", i, "stealed from", j)
         elif name == "assassinate":
             new_state = self.loseCard(new_state, j)
-            print("player", i, "assassinated", j)
+            #print("player", i, "assassinated", j)
         else:
-            print("player", i, "blocked foreign aid of", j)
+            pass
+            #print("player", i, "blocked foreign aid of", j)
         return (new_state[0], tuple(new_coins), None, False)
 
 
@@ -156,6 +158,42 @@ class Game:
 
     def chooseRandomAction(self, actions):
         return random.choice(actions)
+
+    def chooseBaseLineAction(self,actions,state):
+        if actions[0][0] == "doubt":
+            eps = 0.2
+            if state[0][0][0][0] == state[0][0][0][0] and state[2][0] in self.card_functions[state[0][0][0][0]]:
+                return actions[0]
+            if random.random() < eps:
+                return actions[0]
+            else:
+                return actions[1]
+        else:
+            available_functions = []
+            for card in state[0][0]:
+                if card[1] == 1:
+                    available_functions += self.card_functions[card[0]]
+            truth_actions = []
+            fake_actions = []
+            for action in actions:
+                if action[0] in available_functions:
+                    truth_actions.append(action)
+                elif action[0] == "income" or action[0] == "foreign_aid" or action[0] == "no_block" or action[0] == None:
+                    truth_actions.append(action)
+                elif  action[0] == "coup":
+                    truth_actions.append(action)
+                else:
+                    fake_actions.append(action)
+            eps = 0.2
+            #print(truth_actions,fake_actions)
+            if len(fake_actions) == 0:
+                return random.choice(truth_actions)
+            if len(truth_actions) == 0:
+                return random.choice(fake_actions)
+            if random.random() < eps:
+                return random.choice(fake_actions)
+            else:
+                return random.choice(truth_actions)
 
     def succ(self, action, state):
         i = action[1]
@@ -210,7 +248,7 @@ class Game:
             new_state = (state[0], tuple(new_coins), action, False)
         return new_state, new_player
 
-    def printState(self):
+    def printState(self, current_player):
         state = self.game_state
         print("Current state:")
         for i,cards in enumerate(state[0]):
@@ -218,6 +256,8 @@ class Game:
             for card in cards:
                 print(card[0], card[1])
             print("number of coins:", state[1][i])
+        print("current action:", state[2], state[3])
+        print("current player", current_player)
 
 
     def simulateGame(self):
@@ -230,24 +270,33 @@ class Game:
                 each_player_cards.append(add_card)
             player_cards.append(tuple(each_player_cards))
         self.game_state = (tuple(player_cards), tuple([2 for i in range(self.num_players)]), None, False)
-        print("initial state: ", self.game_state)
+        #print("initial state: ", self.game_state)
         current_player = self.starting_player
         while True:
             actions = self.getActions(current_player, self.game_state)
             if len(actions) == 0:
                 cur_action = self.game_state[2]
-                if cur_action[0] == "tax" or cur_action[0] == "foreign_aid":
-                    new_player = self.getNextLivingPlayer(self.game_state[2][1])
-                    new_state = self.takeEffect(self.game_state, self.game_state[2])
-                elif len(cur_action[0]) > 5 and cur_action[0][:5] == "block":
-                    new_player = self.getNextLivingPlayer(self.game_state[2][2])
-                    new_state = (self.game_state[0], self.game_state[1], None, False)
-                    print("Player", cur_action[1], "blocked player", cur_action[2])
+                if not self.game_state[2]:
+                    if cur_action[0] == "tax" or cur_action[0] == "foreign_aid":
+                        new_player = self.getNextLivingPlayer(self.game_state[2][1])
+                        new_state = self.takeEffect(self.game_state, self.game_state[2])
+                    elif len(cur_action[0]) > 5 and cur_action[0][:5] == "block":
+                        new_player = self.getNextLivingPlayer(self.game_state[2][2])
+                        new_state = (self.game_state[0], self.game_state[1], None, False)
+                        #print("Player", cur_action[1], "blocked player", cur_action[2])
+                    else:
+                        new_player = cur_action[2]
+                        new_state = (self.game_state[0], self.game_state[1], cur_action, True)
                 else:
-                    new_player = cur_action[2]
-                    new_state = (self.game_state[0], self.game_state[1], cur_action, True)
+                    new_state = self.takeEffect(self.game_state, self.game_state[2])
+                    new_player = self.getNextLivingPlayer(self.game_state[2][1])
             else:
-                action = self.chooseRandomAction(actions)
+                if current_player == 0:
+                    #action = self.chooseBaseLineAction(actions, self.game_state)
+                    action = self.chooseRandomAction(actions)
+                else:
+                    action = self.chooseRandomAction(actions)
+                """
                 if action[0] == "no_doubt":
                     print("Player", action[1], "chose to not doubt")
                 elif action[0] == "no_block":
@@ -256,27 +305,33 @@ class Game:
                     print("Player", action[1], "chose to not block")
                 else:
                     print("Player", action[1], "chose action", action[0], "against", action[2])
+                """
                 new_state, new_player = self.succ(action, self.game_state)
             self.game_state = new_state
             current_player = new_player
-            self.printState()
-            input("")
+            #self.printState(current_player)
+            #input("")
             if self.isDead(current_player):
                 current_player = self.getNextLivingPlayer(current_player)
             #print("Current state: ", self.game_state)
             if self.isEnd():
                 winner = self.getNextLivingPlayer(current_player)
-                print("Player", winner, "wins!")
+                #print("Player", winner, "wins!")
                 break
-        return self.game_state
+        return winner
 
 
 def main():
-    cards = [("duke",1), ("duke",1),("assassin",1),("assassin",1),("contessa",1),("contessa",1),("captain",1),("captain",1),("ambassador",1),("ambassador",1),]
+    cards = [("duke",1), ("duke",1),("assassin",1),("assassin",1),("contessa",1),("contessa",1),("captain",1),("captain",1),("ambassador",1),("ambassador",1)]
     game = Game(cards)
-    game.reset()
+    counts = collections.defaultdict(int)
+    for i in range(1000):
+        game.reset()
+        winner = game.simulateGame()
+        counts[winner] += 1
+        print("Game", i+1, "ends, Player", winner, "wins.")
+    print(counts)
 
-    game.simulateGame()
 
 if __name__ == "__main__":
     main()
